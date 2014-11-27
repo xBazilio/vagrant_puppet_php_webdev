ead41a952abd58daf2c36e6959df2de894ae3132
Bazilio <x.bazilio@gmail.com>
2014-11-27 14:51:24 +0300

    fixed path error

--- puppet/modules/dev_tools/manifests/init.pp
+++ puppet/modules/dev_tools/manifests/init.pp
@@ -6,7 +6,7 @@ class dev_tools {
 
     exec { 'composer_install':
         command => 'curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer',
-        path    => '/usr/bin:/usr/sbin',
+        path    => '/usr/bin:/usr/sbin:/bin',
         require => Package['curl'],
     }
 }
